751f300ce5978c7eb861ad75080e8e48d0ef33af
N0ViP <01.xx.vip@gmail.com>
2025-01-12 09:20:55 +0100

    added powx-n

---
+++ powx-n.c
@@ -0,0 +1,21 @@
+double myPow(double x, int n) {
+    
+    double res = 1;
+    long N = n; 
+    
+    if (n < 0)
+    {
+        x = 1 / x;        
+        N = -N;   
+    }
+
+    while (N > 0)
+    {
+        if (N % 2 == 1)
+            res = res * x;
+        x = x * x;
+        N /= 2;
+    }
+    
+    return (res);
+}
