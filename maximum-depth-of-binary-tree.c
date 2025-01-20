549be6de8ea2ea3d9e6ad4453feb8791c54fc00e
N0ViP <01.xx.vip@gmail.com>
2025-01-20 22:46:38 +0100

    added maximum-depth-of-binary-tree

---
+++ maximum-depth-of-binary-tree.c
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+int maxDepth(struct TreeNode* root) {
+    if (!root)
+        return (0);
+    else
+    {
+        int right = maxDepth(root->right);
+        int left = maxDepth(root->left);
+        if (right > left)
+            return (right + 1);
+        return (left + 1);
+    }
+    
+}
