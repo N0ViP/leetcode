311f3b9dc35937d531fb5e11d430aee7a3d323f2
N0ViP <01.xx.vip@gmail.com>
2025-01-23 04:20:40 +0100

    added binary_search

---
+++ binary_search.c
@@ -0,0 +1,22 @@
+int searchInsert(int* nums, int numsSize, int target) {
+
+    int left = 0, right = numsSize - 1, mid;
+
+    while (left <= right)
+    {
+        mid = ((right - left) / 2) + left;
+        if (nums[mid] == target)
+            return (mid);
+        if (target > nums[mid])
+        {
+            left = mid + 1;
+        }
+        else
+        {
+            right = mid - 1;
+        }
+        
+    }
+
+    return (mid);
+}
